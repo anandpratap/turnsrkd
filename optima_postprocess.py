8cb4cfade0f046bd26881249c2df7b123172f1dd
Anand <anandps@umich.edu>
2015-09-02 13:35:29 -0400

    Added another class which generates rundir, submits jobs and collect jacobian. The MPI one does not work because of subprocess issue with mpi

--- optima_postprocess.py
+++ optima_postprocess.py
@@ -1,10 +1,102 @@
 import numpy as np
 from mpi4py import MPI
-from driver import AdTurnsRKD, SensTurnsRKD
+from driver import TurnsRKD, AdTurnsRKD, SensTurnsRKD
 import subprocess as sp
 import os
 import scipy.io as io
+import time
+PBS_STRING="""
+#!/bin/sh
+#PBS -S /bin/sh
+#PBS -N jac_%i
+#PBS -l nodes=1:ppn=1,pmem=2g
+#PBS -l walltime=2:00:00
+#PBS -A kdur_fluxoe
+#PBS -l qos=flux
+#PBS -q fluxoe
+#PBS -V
+#
+# echo "I ran on:"
+# cat $PBS_NODEFILE
+#
+# Create a local directory to run from and copy your files to it.
+# Let PBS handle your output
+export OMPI_MCA_mtl=^mxm
+export OMPI_MCA_coll_fca_enable=0
+export OMPI_MCA_btl_vader_single_copy_mechanism=none
+export OMPI_MCA_btl_sm_use_knem=0
+export ADOVERTURNS2D=/scratch/kdur_fluxoe/anandps/turns2d-new/bin/adoverturns2d
+#  Include the next three lines always
+if [ "x${PBS_NODEFILE}" != "x" ] ; then
+   cat $PBS_NODEFILE   # contains a list of the CPUs you were using
+fi
+
+#  Change to the directory you submitted from
+if [ -n "$PBS_O_WORKDIR" ]; then cd $PBS_O_WORKDIR; fi
+
+# Let PBS handle your output
+$ADOVERTURNS2D < unsteady
+
+"""
 class JacobianCalc(object):
+    def __init__(self):
+        self.solution_dir = ""
+        self.OBJINP_STR = ""
+        self.copytree = ""
+        self.points_index = []
+
+    def generate_rundir(self):
+        n = len(self.points_index)
+        print "Generating directories"
+        for idx, point in enumerate(self.points_index):
+            print "\r point %i"%point,
+            adjoint_dir = os.path.join(self.solution_dir, "jac_adjoint_%i"%point)
+            os.mkdir(adjoint_dir)
+            os.chdir(adjoint_dir)
+            for f in self.copytree:
+                file_ = os.path.join(self.solution_dir, f)
+                sp.call("cp " + file_ + " " + adjoint_dir, shell=True)
+                
+            f = open("obj.inp", "w")
+            f.write(self.OBJINP_STR%point)
+            f.close()
+            f = open("run.pbs", "w")
+            f.write(PBS_STRING%point)
+            f.close()
+            os.chdir(self.solution_dir)
+        print "\n"
+        
+    def submit_job(self):
+        for idx, point in enumerate(self.points_index):
+            current_adjoint_rundir = os.path.join(self.solution_dir, "jac_adjoint_%i"%point)
+            os.chdir(current_adjoint_rundir)
+            os.system("qsub run.pbs")
+            os.chdir(self.solution_dir)
+            time.sleep(0.1) 
+            print "\r Submitting pbs job for point %i"%point,
+        print "\n"
+
+    def collect_jac(self):
+        n = len(self.points_index)
+        turns = TurnsRKD(nspec=1, rundir = self.solution_dir)
+        nj, nk = turns.get_grid_dimensions()
+        nt = nj*nk
+        jac_map = np.zeros([n, nt])
+        F_map = np.zeros([n])
+        
+        for idx, point in enumerate(self.points_index):
+            print "\r Creating jacobian for point %i"%point,
+            current_adjoint_rundir = os.path.join(self.solution_dir, "jac_adjoint_%i"%point)
+            sensturns = SensTurnsRKD(nspec=1, rundir = current_adjoint_rundir)
+            sens = sensturns.get_beta_sensitivity()
+            jac_map[idx, :] = np.reshape(sens, [1, nt])
+            F_map[idx] = np.loadtxt('fort.747')
+        print "\n"
+        beta_map = np.reshape(sensturns.read_beta("beta.opt"), [nt, 1])
+        io.savemat(os.path.join(self.solution_dir, "map.mat"),  {'jac_map':jac_map, 'beta_map':beta_map, 'F_map': F_map, 'nj':nj, 'nk':nk})
+        
+class JacobianCalcMPI(object):
+    # does not work, since subprocess causes some issue with mpi4py
     def __init__(self):
         self.solution_dir = ""
         self.OBJINP_STR = ""
